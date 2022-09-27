3765936ee0bec05876ab68d504ee152e377e3b21
Esteban Arias <esteban.arfu@icloud.com>
2022-09-26 22:08:14 -0600

    Diseños, enemigos y respawns

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,12 +12,14 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rigidBody2D;
     private Animator animator;
+    private Vector2 initialPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -26,20 +28,23 @@ public class Player : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal") * speed; //Esto detecta si presionamos las teclas para mover al personaje.
         if(horizontal < 0.0f)
         {
-            transform.localScale = new Vector2(-20.0f, 20.0f); 
+            transform.localScale = new Vector2(-1.0f, 1.0f); 
         }
         else if(horizontal > 0.0f)
         {
-            transform.localScale = new Vector2(20.0f, 20.0f); 
+            transform.localScale = new Vector2(1.0f, 1.0f); 
 
         }
 
         animator.SetBool("isRunning", horizontal != 0.0f);
 
         Debug.DrawRay(transform.position, Vector2.down * 1.1f, Color.blue);
-        if(Physics2D.Raycast(transform.position, Vector2.down, 1.1f)){
+        if(Physics2D.Raycast(transform.position, Vector2.down, 1.1f))
+        {
             isGrounded = true;
-        }else
+            animator.SetBool("isJumping", false);
+        }
+        else
         {
             isGrounded = false;
         }
@@ -48,11 +53,28 @@ public class Player : MonoBehaviour
             Jump();
         }
 
+        DeathOnFall();
+
+    }
+
+    public void Death()
+    {
+        transform.position = initialPosition;
     }
 
     private void Jump()
     {
+        animator.SetBool("isJumping", true);
         rigidBody2D.AddForce(Vector2.up * jumpForce);
+    }
+
+    private void DeathOnFall()
+    {
+        if(transform.position.y < -10f)
+        {
+            //transform.position = initialPosition;
+            Death();
+        }
     }
 
     private void FixedUpdate(){
